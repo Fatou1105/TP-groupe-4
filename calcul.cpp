af0d9d6dab6ac892d6f774150f6a91928546ce84
Fatou1105 <fatou.mbengue1105@gmail.com>
2025-06-14 12:06:05 +0000

    Update calcul.cpp

--- calcul.cpp
+++ calcul.cpp
@@ -1,1 +1,19 @@
-//
+//directive de compilation par Alix
+
+
+//fonction de soustraction par Cynthia
+
+
+//fonction de multiplication par Fatou
+
+
+
+//fonction de  division par Alix
+
+
+
+//fonction d'addition par Cynthia
+
+
+
+//methode main par Fatou
